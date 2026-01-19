3f8121b17377c26e466d277d2373bb6decde544a
Brandon_Pacchioni <paccbrandon80@gmail.com>
2026-01-19 16:55:22 -0500

    Update models.py

--- core/models.py
+++ core/models.py
@@ -29,6 +29,15 @@ class Loan(models.Model):
 
     def __str__(self):
         return f"{self.book.title} - {self.user.username}"
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rating = models.IntegerField()  # 1 a 5
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.book.title} - {self.user.username} ({self.rating}/5)"
 
         
     
